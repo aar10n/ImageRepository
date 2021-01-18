bb5443438eca1f965069b9e3617ae8e629364c55
Aaron Gill-Braun <aarongillbraun@gmail.com>
2021-01-17 23:07:00 -0500

    Title & description is now editable

--- client/src/views/Image/index.tsx
+++ client/src/views/Image/index.tsx
@@ -1,8 +1,8 @@
-import { useEffect } from 'react';
+import { KeyboardEvent, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
-import { fetchImage } from 'redux/image/actions';
+import { editImage, fetchImage } from 'redux/image/actions';
 import { getImage, isOwner } from 'redux/image/selectors';
 
 interface Params {
@@ -28,33 +28,64 @@ const useStyles = makeStyles(() =>
       flexDirection: 'column',
       alignItems: 'center',
     },
+
     title: {
+      WebkitUserSelect: 'none',
       width: '100%',
       textAlign: 'left',
       fontFamily: 'Helvetica, Arial',
       fontSize: '30px',
-      fontWeight: 600,
       marginBottom: '5px',
+      fontWeight: 600,
+      cursor: 'text',
     },
     description: {
       width: '100%',
       textAlign: 'left',
       fontFamily: 'Helvetica, Arial',
-      fontSize: '16px',
+      fontSize: '18px',
       marginTop: '8px',
+      fontWeight: 600,
+      cursor: 'text',
+    },
+
+    editableText: {
+      width: '100%',
+      display: 'inline-block',
+      paddingLeft: '1px',
+      outline: 'none',
+      textDecoration: 'none',
+      '&:empty::before': {
+        color: '#63656D',
+        fontStyle: 'italic',
+        content: 'attr(placeholder)',
+        display: 'inline-block',
+      },
     },
     image: {
       width: '100%',
+    },
+
+    hiddenInputContainer: {
+      position: 'relative',
+      overflow: 'hidden',
+    },
+    hiddenInput: {
+      position: 'absolute',
+      top: -500,
     },
   })
 );
 
 export const Image = () => {
+  const [text, setText] = useState('');
   const { id } = useParams<Params>();
   const dispatch = useDispatch();
   const image = useSelector(getImage(id));
   const owner = useSelector(isOwner(id));
   const classes = useStyles();
+  const titleRef = useRef<HTMLSpanElement>(null);
+  const descRef = useRef<HTMLSpanElement>(null);
 
   useEffect(() => {
     if (!image) {
@@ -62,7 +93,25 @@ export const Image = () => {
     }
   }, [dispatch, id, image]);
 
-  console.log(`is owner = ${owner} | ${id}`);
+  const handleKeypress = (
+    event: KeyboardEvent<HTMLSpanElement>,
+    field: 'title' | 'description'
+  ) => {
+    const { key } = event;
+    if (key === 'Enter') {
+      const text = event.currentTarget.innerText;
+      event.currentTarget.blur();
+      event.preventDefault();
+
+      if (image && text !== image?.[field]) {
+        dispatch(editImage(image.id, { [field]: text }));
+      }
+    } else if (key === 'Escape') {
+      event.currentTarget.innerText = text;
+      event.currentTarget.blur();
+      event.preventDefault();
+    }
+  };
 
   return (
     image && (
@@ -70,9 +119,24 @@ export const Image = () => {
         <div className={classes.content}>
           <div className={classes.title}>
             <span
+              className={owner ? classes.editableText : ''}
               contentEditable={owner}
+              suppressContentEditableWarning={true}
               placeholder="Title..."
-              onChange={event => console.log(event)}
+              ref={titleRef}
+              onKeyDown={event => handleKeypress(event, 'title')}
+              onFocus={event => {
+                setText(event.currentTarget.innerText);
+              }}
+              onBlur={event => {
+                // this is the the only way i've found to fix the
+                // issue of the cursor jumping to the beginning of
+                // the div when unfocusing.
+                event.currentTarget.contentEditable = 'false';
+                setTimeout(() => {
+                  titleRef.current!.contentEditable = 'true';
+                }, 5);
+              }}
             >
               {image.title}
             </span>
@@ -82,7 +146,27 @@ export const Image = () => {
             src={image.url}
             alt={image.title ?? image.description ?? image.id}
           />
-          <div className={classes.description}>{image.description}</div>
+          <div className={classes.description}>
+            <span
+              className={owner ? classes.editableText : ''}
+              contentEditable={owner}
+              suppressContentEditableWarning={true}
+              placeholder="Title..."
+              ref={descRef}
+              onKeyDown={event => handleKeypress(event, 'description')}
+              onFocus={event => {
+                setText(event.currentTarget.innerText);
+              }}
+              onBlur={event => {
+                event.currentTarget.contentEditable = 'false';
+                setTimeout(() => {
+                  descRef.current!.contentEditable = 'true';
+                }, 5);
+              }}
+            >
+              {image.description}
+            </span>
+          </div>
         </div>
       </div>
     )
